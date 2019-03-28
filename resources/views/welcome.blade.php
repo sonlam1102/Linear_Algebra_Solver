075633fe777a11852524d618a0b4df5ef277a5a3
NguyenThiThuc97 <ndtt8797@gmail.com>
2019-03-29 01:46:50 +0700

    config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/png" href="http://sheaves.github.io/images/favicons/android-icon-192x192.png"/>
 
         <title>Đại Số Tuyến Tính</title>
 
@@ -67,7 +68,7 @@
             }
 
             .border-frm{
-                border: 2px solid black;
+                border: 1px solid black;
                 border-radius: 5px;
                 height: 450px;
             }
@@ -80,49 +81,75 @@
             .mr-top-40{
                 margin-top:40px;
             }
+            .mr-top-10{
+                margin-top:10px;
+            }
             .navbar-cus{
                 background-color: #0080ff;
             }
             li.text-navbar a {
                 color: #000;
             }
-            li a hover{
-                background-color: 936c6c
+            li a.operator:hover{
+                box-shadow: 0px 1px 0 0px blue inset, 0px -1px 0 0px blue inset;
+                background-color: #0080ff
             }
             .right-title{
                 text-align: center;
             }
+            .mr-10{
+                margin:10px;
+            }
+            .pd-50{
+                padding: 50px
+            }
+            .border-input{
+                border: 1px solid black;
+                border-radius: 5px;
+            }
+            #navbarSupportedContent{
+                color: black
+            }
+            div.upfile {
+              position: relative;
+              overflow: hidden;
+            }
+            input.upfile {
+              position: absolute;
+              font-size: 50px;
+              opacity: 0;
+              right: 0;
+              top: 0;
+            }   
         </style>
     </head>
     <body>
-        <div class="">
-            <div style="width: 100%;height: 100px">
+        <div>
+            <div class="container">
                 <div class="row justify-content-between">
-                    <div class="col-sm-6">
-                        <img src="http://utc2.edu.vn//uploads/img/images/logo.png"/>
+                    <div class="col-sm-4">
+                        <img class="mr-10" src="http://utc2.edu.vn//uploads/img/images/logo.png"/>
                     </div>
                     <div class="col-sm-6">
-                        <label class="" style="font-size:30px;text-align: center; text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;">CHƯƠNG TRÌNH HỖ TRỢ GIẢI CÁC BÀI TẬP <br/> KIẾN THỨC ĐẠI SỐ TUYẾN TÍNH</label>
+                        <label class="mr-top-20" style="font-size:25px;text-align: center; text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;">CHƯƠNG TRÌNH HỖ TRỢ GIẢI CÁC BÀI TẬP <br/> KIẾN THỨC ĐẠI SỐ TUYẾN TÍNH</label>
                     </div>
                 </div>
-                
-                
             </div>
             <nav class="navbar navbar-expand-lg navbar-cus">
-              <a class="navbar-brand" href="#" style="color: #000"><i class="fas fa-home"></i></a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
+                <a class="navbar-brand" href="#" style="color: #000"><i class="fas fa-home"></i></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item dropdown text-navbar">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Định thức ma trận</a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a class="dropdown-item get-operator" href="#">Action</a>
-                          <a class="dropdown-item get-operator" href="#">Another action</a>
+                          <a class="dropdown-item get-operator operator" href="#">Action</a>
+                          <a class="dropdown-item get-operator operator" href="#">Another action</a>
                           <!-- <div class="dropdown-divider"></div> -->
-                          <a class="dropdown-item get-operator" href="#">Something else here</a>
+                          <a class="dropdown-item get-operator operator" href="#">Something else here</a>
                         </div>
                     </li>
                     <li class="nav-item active text-navbar">
@@ -132,10 +159,10 @@
                     <li class="nav-item dropdown text-navbar">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Không gian vector</a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a class="dropdown-item get-operator" href="#">Action</a>
-                          <a class="dropdown-item get-operator" href="#">Another action</a>
+                          <a class="dropdown-item get-operator operator" href="#">Action</a>
+                          <a class="dropdown-item get-operator operator" href="#">Another action</a>
                           <!-- <div class="dropdown-divider"></div> -->
-                          <a class="dropdown-item get-operator" href="#">Something else here</a>
+                          <a class="dropdown-item get-operator operator" href="#">Something else here</a>
                         </div>
                     </li>
                 </ul>
@@ -143,38 +170,43 @@
                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
-              </div>
+            </div>
             </nav>
-            <div class="" style="margin: 50px;">
+            <div style="margin:10px 50px;">
                 <form>
                      <div class="row justify-content-between">
                         <div class="col-sm-4 border-frm" style="width: 100%;color: blue;">
                             <label class="mr-top-20">Dạng bài toán</label>
-                            <input type="text" name="" class="form-control" value="" id="operator">
-                            <button class="btn btn-success mr-top-20">Import file</button><br/>
-                            <textarea class="form-control mr-top-20" id="exampleFormControlTextarea1" rows="10"></textarea>
+                            <input type="text" name="" class="form-control border-input" value="" id="operator" readonly>
+                            <div class="file btn btn-success upfile mr-top-20" id="upfile">
+                                Upload
+                                <input type="file" name="file" class="upfile" id="upfiles"/>
+                            </div>
+                            <textarea class="form-control mr-top-20 border-input" id="exampleFormControlTextarea1" rows="10"></textarea>
                         </div>
-                        <div class="col-sm-1 mr-top-200">
-                            <button class="btn btn-success">Excute</button><br/>
+                        <div class="col-sm-1">
+                            <button class="btn btn-success mr-5"><i class="fas fa-angle-double-right"></i></button><br/>
                         </div>
-                        <div class="col-sm-7 border-frm">
+                        <div class="col-sm-7 border-frm" id="show">
                         </div>
                     </div>
                 </form>
             </div>
         </div>
         <hr/>
-        <div class="container">
-            <div style="color: #000;">
-                TRƯỜNG ĐH GIAO THÔNG VẬN TẢI - PHÂN HIỆU TẠI TP. HCM<br/>
+        <div class="pd-50" style="background-color: lightseagreen; color: black">
+            TRƯỜNG ĐH GIAO THÔNG VẬN TẢI - PHÂN HIỆU TẠI TP. HCM<br/>
             Địa chỉ: 450-451 Lê Văn Việt, Phường Tăng Nhơn Phú A, Quận 9, TP. Hồ Chí Minh<br/>
             Điện thoại: (028).3896.6798 - (028).7300.1155 - Email: banbientap@utc2.eu.vn<br/>
             Fax: (028).3896.4736 - Website: http://utc2.edu.vn
-            </div>
         </div>
         <script type="text/javascript">
             $(".get-operator").click(function(){
                 $("#operator").val($(this).text());
+            });
+
+            $("#upfiles").click(function() {
+                
             });
         </script>
     </body>
