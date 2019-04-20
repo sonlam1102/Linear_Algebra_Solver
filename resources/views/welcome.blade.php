74c774b28396e213ae5f506cde8d7640196b3173
NguyenVanToan17121997 <NguyenVanToan17121997@gmail.com>
2019-04-20 11:00:41 +0700

    config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -153,7 +153,6 @@
             $(document).ready(function() {
                 //search execute
                 let optionsarray = [
-
                     {
                         "name": "",
                         "type": ""
